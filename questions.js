65c5bac0d3463f166bdcd3b2004baa0a80f2567d
dhiakhalfaoui <dhiakhafaloui737@gmail.com>
2023-08-25 10:34:30 +0100

    final

---
+++ questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What is the primary purpose of CSS (Cascading Style Sheets) in web development?",
+      answer: "4) Controlling the visual presentation of a web page",
+      options: [
+        "1) Adding interactivity to web pages",
+        "2) Defining the structure of a web page",
+        "3) Storing and managing data on the server",
+        "4) Controlling the visual presentation of a web page"
+      ]
+    },
+    {
+      numb: 2,
+      question: "In CSS, which property is used to control the spacing between individual letters in a text?",
+      answer: "3) letter-spacing",
+      options: [
+        "1) text-align ",
+        "2) line-height",
+        "3) letter-spacing",
+        "4) word-spacing"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Which CSS property is used to change the text color of an element?",
+      answer: "1) color",
+      options: [
+        "1) color",
+        "2) text-color",
+        "3) font-color",
+        "4) text-style"
+      ]
+    },
+    {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "2) Cascading Style Sheets",
+      options: [
+        "1) Creative Style Sheets",
+        "2) Cascading Style Sheets",
+        "3) Computer Style Sheets",
+        "4) Colorful Style Sheets"
+      ]
+    },
+    {
+      numb: 5,
+      question: "In JavaScript, which keyword is used to declare a variable?",
+      answer: "3) let",
+      options: [
+        "1) variable",
+        "2) vrb",
+        "3) let",
+        "4) declare"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Which built-in function is used to print content to the console in JavaScript?",
+      answer: "1) console.log()",
+      options: [
+        "1) console.log()",
+        "2) print()",
+        "3) display()",
+        "4) log()"
+      ]
+    },
+    {
+      numb: 7,
+      question: "What does the acronym HTML stand for in web development?",
+      answer: "3) Hyper Text Markup Language",
+      options: [
+        "1)  Hyperlink and Text Markup Language",
+        "2) Hyper Transfer Markup Language",
+        "3) Hyper Text Markup Language",
+        "4) High-Level Textual Markup Language"
+      ]
+    },
+    {
+      numb: 8,
+      question: "In CSS, which property is used to control the spacing between elements?",
+      answer: "2) margin",
+      options: [
+        "1) space",
+        "2) margin",
+        "3) padding",
+        "4) spacing"
+      ]
+    },
+    {
+      numb: 9,
+      question: "What does the acronym API stand for in web development?",
+      answer: "3) Application Programming Interface",
+      options: [
+        "1) Advanced Program Interaction",
+        "2) Application Process Integration",
+        "3) Application Programming Interface",
+        "4) Advanced Protocol Integration"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Which operator is used to combine two or more strings in JavaScript?",
+      answer: "1) +",
+      options: [
+        "1) +",
+        "2) *",
+        "3) &",
+        "4) %"
+      ]
+    }
+  ]
+  
